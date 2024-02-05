5f2872bdd3dc6445b453a6d968dc35f3ea430828
Gaétan Collaud <gaetancollaud@gmail.com>
2024-02-05 15:32:47 +0100

    set query as permit all

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kafka-cost-control
+
+This tools help you understand the cost of your Kafka clusters. It will give you insights on what is costing you and where you could optimize your costs.
+
+## Documentation
+ - [Demo](TODO)
+ - [User manual](https://spoud.github.io/kafka-cost-control/#_user_manual)
+ - [Installation guide](https://spoud.github.io/kafka-cost-control/#section-installation)
+ - [Architecture](https://spoud.github.io/kafka-cost-control/#_architecture)
--- aggregator/src/main/java/io/spoud/kcc/aggregator/graphql/PricingRulesResource.java
+++ aggregator/src/main/java/io/spoud/kcc/aggregator/graphql/PricingRulesResource.java
@@ -9,42 +9,44 @@ import io.spoud.kcc.aggregator.repository.MetricNameRepository;
 import io.spoud.kcc.aggregator.repository.PricingRulesRepository;
 import io.spoud.kcc.aggregator.stream.KafkaStreamManager;
 import io.spoud.kcc.data.PricingRule;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.streams.state.KeyValueIterator;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.NonNull;
 import org.eclipse.microprofile.graphql.Query;
 
+import java.util.List;
+
 @GraphQLApi
 @RequiredArgsConstructor
-@Authenticated
 public class PricingRulesResource {
-  private final KafkaStreamManager kafkaStreamStarter;
-  private final PricingRulesRepository pricingRulesRepository;
-  private final MetricNameRepository metricNameRepository;
+    private final PricingRulesRepository pricingRulesRepository;
+    private final MetricNameRepository metricNameRepository;
 
-  @Query("metricNames")
-  public @NonNull List<@NonNull MetricNameEntity> metricNames() {
-    return metricNameRepository.getMetricNames();
-  }
+    @PermitAll
+    @Query("metricNames")
+    public @NonNull List<@NonNull MetricNameEntity> metricNames() {
+        return metricNameRepository.getMetricNames();
+    }
 
-  @Query("pricingRules")
-  public @NonNull List<@NonNull PricingRuleEntity> pricingRules() {
-      return pricingRulesRepository.getPricingRules();
-  }
+    @PermitAll
+    @Query("pricingRules")
+    public @NonNull List<@NonNull PricingRuleEntity> pricingRules() {
+        return pricingRulesRepository.getPricingRules();
+    }
 
-  @Mutation("savePricingRule")
-  public @NonNull PricingRuleEntity savePricingRule(PricingRuleSaveRequest request) {
-    final PricingRule saved = pricingRulesRepository.save(request.toAvro());
-    return PricingRuleEntity.fromAvro(saved);
-  }
+    @Authenticated
+    @Mutation("savePricingRule")
+    public @NonNull PricingRuleEntity savePricingRule(PricingRuleSaveRequest request) {
+        final PricingRule saved = pricingRulesRepository.save(request.toAvro());
+        return PricingRuleEntity.fromAvro(saved);
+    }
 
-  @Mutation("deletePricingRule")
-  public PricingRuleEntity deletePricingRule(PricingRuleDeleteRequest request) {
-    final PricingRule deleted = pricingRulesRepository.deletePricingRule(request.metricName());
-    return PricingRuleEntity.fromAvro(deleted);
-  }
+    @Authenticated
+    @Mutation("deletePricingRule")
+    public PricingRuleEntity deletePricingRule(PricingRuleDeleteRequest request) {
+        final PricingRule deleted = pricingRulesRepository.deletePricingRule(request.metricName());
+        return PricingRuleEntity.fromAvro(deleted);
+    }
 }
