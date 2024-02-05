c1b54e0dc1f67acf636b2eb2b88d6c23da994d3c
Gaétan Collaud <gaetancollaud@gmail.com>
2024-02-05 14:18:48 +0100

    try to fix build

--- docs/build.sh
+++ docs/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-docker run -d --name plantuml -p 8081:8080 plantuml/plantuml-server:jetty
 
-export PLANTUML_URL="http://localhost:8081"
+asciidoctor -r asciidoctor-diagram README.adoc
 
-asciidoctor -r asciidoctor-plantuml README.adoc
-
-docker rm -f plantuml
