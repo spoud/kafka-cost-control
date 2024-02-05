b107a6fc73484d2c5c19f4bb64afd921e96780dc
Gaétan Collaud <gaetancollaud@gmail.com>
2024-02-05 13:29:17 +0100

    rename doc file

--- docs/build.sh
+++ docs/build.sh
@@ -4,6 +4,6 @@ docker run -d --name plantuml -p 8081:8080 plantuml/plantuml-server:jetty
 
 export PLANTUML_URL="http://localhost:8081"
 
-asciidoctor -r asciidoctor-plantuml index.adoc
+asciidoctor -r asciidoctor-plantuml README.adoc
 
 docker rm -f plantuml
