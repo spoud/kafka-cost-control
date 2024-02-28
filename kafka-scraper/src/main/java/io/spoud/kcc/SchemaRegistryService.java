8eeedb52c04a0d3f55fe6790b5042f53d0713598
Alexander Rovner <alexander.rovner@mgb.ch>
2024-02-28 16:41:47 +0100

    remove redundant header

--- kafka-scraper/src/main/java/io/spoud/kcc/SchemaRegistryService.java
+++ kafka-scraper/src/main/java/io/spoud/kcc/SchemaRegistryService.java
@@ -3,14 +3,12 @@ package io.spoud.kcc;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import java.util.Set;
 
 @Path("/schemas")
-@ClientHeaderParam(name = "Authorization", value = "Basic Base64codedcredentials")
 @RegisterRestClient(configKey = "schema-registry-api")
 @RegisterClientHeaders(ConfluentAuthHeader.class)
 public interface SchemaRegistryService {
