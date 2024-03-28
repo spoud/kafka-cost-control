4bb891ac5f412e73a4dc859535ea60f3cea3bbf4
Alexander Rovner <alexander.rovner@mgb.ch>
2024-03-28 12:32:15 +0100

    add integration test to verify that containerized app launches

---
+++ aggregator/src/test/java/io/spoud/kcc/aggregator/stream/AppStartsTestIT.java
@@ -0,0 +1,11 @@
+package io.spoud.kcc.aggregator.stream;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusIntegrationTest
+public class AppStartsTestIT {
+    @Test
+    public void should_start_in_container() {
+    }
+}
