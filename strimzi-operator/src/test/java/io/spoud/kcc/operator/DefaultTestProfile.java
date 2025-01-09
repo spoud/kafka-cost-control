c8056ac97f58b5e1a1a35fd3863d6ae8994d9c6b
Alexander Rovner <alexander.rovner@mgb.ch>
2025-01-09 10:26:38 +0100

    allow configuring prefixes/suffixes for context regexes

--- strimzi-operator/src/test/java/io/spoud/kcc/operator/DefaultTestProfile.java
+++ strimzi-operator/src/test/java/io/spoud/kcc/operator/DefaultTestProfile.java
@@ -5,10 +5,13 @@ import io.quarkus.test.junit.QuarkusTestProfile;
 import java.util.Map;
 
 public class DefaultTestProfile implements QuarkusTestProfile {
+    public static final String PREFIX = "(eu-west-1[.]|eu-east-1[.])?";
+
     @Override
     public Map<String, String> getConfigOverrides() {
         return Map.of(
                 "cc.strimzi.operator.topics.context-data", "context-data",
+                "cc.strimzi.operator.context-regex-prefix", PREFIX,
                 "quarkus.operator-sdk.crd.apply", "true", // the mock k8s server does not have strimzi CRDs, so we need to apply them
                 "kafka.linger.ms", "0",
                 "quarkus.log.category.\"io.spoud.kcc\".level", "DEBUG",
