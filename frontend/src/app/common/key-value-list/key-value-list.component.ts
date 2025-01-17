51578c5ec41cb1e29d3a984af0eaa64c7e202747
Gaétan Collaud <gaetancollaud@gmail.com>
2025-01-17 16:00:10 +0100

    upgrade angular to version 19 (#303)
    * update angular
    
    * update material

--- frontend/src/app/common/key-value-list/key-value-list.component.ts
+++ frontend/src/app/common/key-value-list/key-value-list.component.ts
@@ -2,11 +2,10 @@ import {Component, input} from '@angular/core';
 import {Entry_String_String} from '../../../generated/graphql/types';
 
 @Component({
-  selector: 'app-key-value-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './key-value-list.component.html',
-  styleUrl: './key-value-list.component.scss'
+    selector: 'app-key-value-list',
+    imports: [],
+    templateUrl: './key-value-list.component.html',
+    styleUrl: './key-value-list.component.scss'
 })
 export class KeyValueListComponent {
 
