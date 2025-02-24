54ec87a4bd128bd0c9648f12022502c72585437d
Lukas Gisi <34534944+lukasgisi@users.noreply.github.com>
2025-02-24 13:40:08 +0100

    GUI: add context creation dialog (#359)
    * GUI: add context creation dialog
    
    * Feedback review: Extract spacing utility css classes; add type to FormGroup
    
    * Use form value for entity type instead of hardcoded one

--- frontend/src/app/tab-context-data/context-data-list/context-data-list.component.ts
+++ frontend/src/app/tab-context-data/context-data-list/context-data-list.component.ts
@@ -6,6 +6,12 @@ import {MatSort, MatSortModule, Sort} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {KeyValueListComponent} from '../../common/key-value-list/key-value-list.component';
+import {MatFabButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {ContextDataCreateComponent} from '../context-data-create/context-data-create.component';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {DatePipe} from '@angular/common';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
     selector: 'app-context-data-list',
@@ -15,10 +21,13 @@ import {KeyValueListComponent} from '../../common/key-value-list/key-value-list.
         MatTableModule,
         MatSortModule,
         KeyValueListComponent,
+        MatFabButton,
+        MatIcon,
+        DatePipe,
+        MatDivider
     ]
 })
 export class ContextDataListComponent implements OnInit, AfterViewInit {
-
 
     @ViewChild(MatSort) sort: MatSort | null = null;
 
@@ -27,10 +36,14 @@ export class ContextDataListComponent implements OnInit, AfterViewInit {
 
     public displayedColumns: string[] = ['creationTime', 'validFrom', 'validUntil', 'entityType', 'regex', 'context'];
 
-    constructor(private contextDataService: GetContextDatasGQL, private _liveAnnouncer: LiveAnnouncer, private _snackBar: MatSnackBar) {
+    constructor(private contextDataService: GetContextDatasGQL, private _liveAnnouncer: LiveAnnouncer, private _snackBar: MatSnackBar, private dialog: MatDialog) {
     }
 
     ngOnInit(): void {
+        this.loadContextData();
+    }
+
+    private loadContextData() {
         this.contextDataService.fetch().subscribe({
             next: value => this.dataSource.data = value.data.contextData,
             error: err => this._snackBar.open("Could not load context data. " + err.message, "close")
@@ -52,5 +65,14 @@ export class ContextDataListComponent implements OnInit, AfterViewInit {
         } else {
             this._liveAnnouncer.announce('Sorting cleared');
         }
+    }
+
+    openCreateDialog() {
+        const dialogRef: MatDialogRef<ContextDataCreateComponent> = this.dialog.open(ContextDataCreateComponent);
+        dialogRef.afterClosed().subscribe(result => {
+            if (result === 'successfully-saved') {
+                this.loadContextData();
+            }
+        });
     }
 }
