ba6a4debb4271e62c131c7192a23c14385c08a8f
Lukas Gisi <34534944+lukasgisi@users.noreply.github.com>
2025-07-15 16:59:29 +0200

    Feature/frontend csv from pie chart (#499)
    * Get CSV also from pie chart (no % yet, only absolute values)
    
    * Get aggregated CSV with each available metric for a given context and timeframe
    
    * Add percentage column, some gui improvement
    
    * Some better distribution for random data, add * 100 for percentage column
    
    * limit decimals in percentage report

--- aggregator/src/main/java/io/spoud/kcc/aggregator/olap/DataExportResource.java
+++ aggregator/src/main/java/io/spoud/kcc/aggregator/olap/DataExportResource.java
@@ -1,20 +1,98 @@
 package io.spoud.kcc.aggregator.olap;
 
 import io.quarkus.logging.Log;
+import io.spoud.kcc.aggregator.graphql.data.MetricHistoryTO;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.StreamingOutput;
 import org.jboss.resteasy.reactive.RestQuery;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.time.Instant;
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Path("/olap/export")
 public class DataExportResource {
     @Inject
     AggregatedMetricsRepository aggregatedMetricsRepository;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    @GET
+    @Path("/aggregated")
+    @Produces("text/csv")
+    public Response aggregatedCsvExport(@RestQuery DateTimeParameter fromDate, @RestQuery DateTimeParameter toDate,
+                                        @RestQuery String groupByContextKey) {
+        Map<String, Collection<MetricHistoryTO>> metricToValue = aggregatedMetricsRepository.exportDataAggregated(
+                Optional.ofNullable(fromDate).map(DateTimeParameter::instant).orElse(null),
+                Optional.ofNullable(toDate).map(DateTimeParameter::instant).orElse(null),
+                groupByContextKey);
+        Map<String, List<Double>> groupByContextToValues = new TreeMap<>();
+        metricToValue.forEach((s, metricHistoryTOs) -> {
+            metricHistoryTOs.forEach(metricHistoryTO -> {
+                groupByContextToValues.compute(
+                        metricHistoryTO.getName(),
+                        (k, v) -> {
+                            if (v == null) {
+                                v = new ArrayList<>();
+                            }
+                            v.add(metricHistoryTO.getValues().getFirst());
+                            return v;
+                        }
+                );
+            });
+        });
+        return Response.ok().entity((StreamingOutput) output -> {
+                    output.write((groupByContextKey + ",").getBytes());
+                    List<String> header = metricToValue.keySet().stream()
+                            .map(s -> s + " [bytes]," + s + " [%]")
+                            .toList();
+                    output.write(String.join(",", header).getBytes());
+                    output.write("\n".getBytes());
+                    List<Double> totals = extractTotals(metricToValue);
+                    groupByContextToValues.forEach((k, v) -> {
+                        List<String> list = new ArrayList<>();
+                        for (int i = 0; i < v.size(); i++) {
+                            Double value = v.get(i);
+                            list.add(value.toString());
+                            list.add(String.format("%.2f", value / totals.get(i) * 100));
+                        }
+                        String line = k + "," + String.join(",", list);
+                        try {
+                            output.write(line.getBytes());
+                            output.write("\n".getBytes());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }).header("Content-Disposition", "attachment; filename=export_" + nowAsIso() + ".csv")
+                .build();
+    }
+
+    private List<Double> extractTotals(Map<String, Collection<MetricHistoryTO>> metricToValue) {
+        List<Double> totals = new ArrayList<>();
+        for (Collection<MetricHistoryTO> value : metricToValue.values()) {
+            Double t = 0.0;
+            for (MetricHistoryTO metricHistoryTO : value) {
+                for (Double metricHistoryTOValue : metricHistoryTO.getValues()) {
+                    t += metricHistoryTOValue;
+                }
+            }
+            totals.add(t);
+        }
+        return totals;
+    }
+
+    private String nowAsIso() {
+        return LocalDateTime.now().format(formatter);
+    }
 
     @GET
     @Produces("text/csv")
