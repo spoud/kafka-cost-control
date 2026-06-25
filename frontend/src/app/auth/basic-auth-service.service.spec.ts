5233ea2458263f53ab38701377e4437ee60722bb
Lukas Gisi <lukas.gisi@spoud.io>
2026-06-25 11:38:42 +0200

    Fix frontend tests; add npm test to CI pipeline

--- frontend/src/app/auth/basic-auth-service.service.spec.ts
+++ frontend/src/app/auth/basic-auth-service.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 import { BasicAuthServiceService } from './basic-auth-service.service';
 
@@ -6,7 +7,9 @@ describe('BasicAuthServiceService', () => {
     let service: BasicAuthServiceService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [ApolloTestingModule],
+        });
         service = TestBed.inject(BasicAuthServiceService);
     });
 
