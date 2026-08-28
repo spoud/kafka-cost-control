87b5b38da63b8463d7137fdf182e5105d9c4986b
GitHub Action <action@github.com>
2026-08-28 12:02:23 +0000

    Build .adoc Files for GitHub Pages from a125181b0d9213f206a404db89c4020a165c98af

--- aggregator/src/main/java/io/spoud/kcc/aggregator/data/Metric.java
+++
@@ -1,9 +0,0 @@
-package io.spoud.kcc.aggregator.data;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
-import io.spoud.kcc.data.EntityType;
-import org.eclipse.microprofile.graphql.NonNull;
-
-// TODO shitty name, find something else
-@RegisterForReflection
-public record Metric(@NonNull EntityType type, @NonNull String objectName) {}
--- aggregator/src/main/java/io/spoud/kcc/aggregator/data/MetricNameEntity.java
+++
@@ -1,7 +0,0 @@
-package io.spoud.kcc.aggregator.data;
-
-import org.eclipse.microprofile.graphql.NonNull;
-
-import java.time.Instant;
-
-public record MetricNameEntity(@NonNull String metricName,@NonNull  Instant lastSeen, @NonNull String aggregationType) {}
