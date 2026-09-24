c9f9a7f6ef74ae5e14f872281a86d69fcecb4320
GitHub Action <action@github.com>
2026-09-24 09:26:32 +0000

    Build .adoc Files for GitHub Pages from ded717fd27acf7e805b7cc66c97cc3e5bd199e4a

--- aggregator/src/main/java/io/spoud/kcc/aggregator/graphql/data/ContextDataDeleteRequest.java
+++
@@ -1,8 +0,0 @@
-package io.spoud.kcc.aggregator.graphql.data;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
-import org.eclipse.microprofile.graphql.NonNull;
-
-@RegisterForReflection
-public record ContextDataDeleteRequest(@NonNull String id) {
-}
--- frontend/src/app/common/filter-bar/filter-bar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-filter-bar',
-    imports: [],
-    templateUrl: './filter-bar.component.html',
-    styleUrl: './filter-bar.component.scss',
-})
-export class FilterBarComponent {}
